c00d93fbbfebf47f0d40e519d62e7319e380692e
UZB BARON <150528348+STARuzb@users.noreply.github.com>
2024-04-27 19:16:09 +0500

    Add files via upload

---
+++ imthon/src/Data.jsx
@@ -0,0 +1,12 @@
+export const userList = [
+  {
+    name: "Hasanboy",
+    email: "Hasanboy@gmail.com",
+    id: 1,
+  },
+  {
+    name: "FN16",
+    email: "FN16@gmail.com",
+    id: 3,
+  },
+];
